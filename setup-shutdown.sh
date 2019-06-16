c4c4c7f3ab1a50035f858a30873e296b6b2cb19e
Mark Kimitch <mark.kimitch@gmail.com>
2019-06-16 14:47:12 -0500

    Using  instead of

--- setup-shutdown.sh
+++ setup-shutdown.sh
@@ -2,20 +2,20 @@
 
 set -e
 
-echo "Downloading shuitdown.py and shutdown.sh ... \n"
+printf "\nDownloading shuitdown.py and shutdown.sh ... \n"
 sudo wget https://cdn.jsdelivr.net/gh/mkimitch/raspberry-pi-scripts@latest/shutdown.py
 sudo wget https://cdn.jsdelivr.net/gh/mkimitch/raspberry-pi-scripts@latest/shutdown.sh
 
-echo "Moving shutdown.py to /usr/local/bin/ ... \n"
+printf "\nMoving shutdown.py to /usr/local/bin/ ... \n"
 sudo mv shutdown.py /usr/local/bin/
 sudo chmod +x /usr/local/bin/shutdown.py
 
-echo "Moving shutdown.sh to /etc/init.d/ ... \n"
+printf "\nMoving shutdown.sh to /etc/init.d/ ... \n"
 sudo mv shutdown.sh /etc/init.d/
 sudo chmod +x /etc/init.d/shutdown.sh
 
-echo "Updating defaults and starting script ... \n"
+printf "\nUpdating defaults and starting script ... \n"
 sudo update-rc.d shutdown.sh defaults
 sudo /etc/init.d/shutdown.sh start
 
-echo "Done!\n"
+printf "\nDone!\n"
